624c06d213c7bb6d17449252a08a73434797c701
lilytimmermans <timmerml@oregonstate.edu>
2023-06-29 21:34:27 -0700

    Initial commit

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Lily Timmermans
+# GitHub username: LilyTimmermans
+# Date: 06/29/2023
+# Description: Asks the user for their favorite animal and then that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
